b0c2ed79876b821c177aa2768d1f9c879440ccea
Kálley Wilkerson <kalleywra@gmail.com>
2023-08-24 14:56:39 -0300

    Adicionando validações personalizadas

--- src/main/java/med/voll/api/domain/paciente/DadosPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DadosPaciente.java
@@ -8,20 +8,20 @@ import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosPaciente(
 
-        @NotBlank
+        @NotBlank(message = "{nome.obrigatorio}")
         String nome,
 
-        @NotBlank
-        @Email
+        @NotBlank(message = "{email.obrigatorio}")
+        @Email(message = "{email.invalido}")
         String email,
 
-        @NotBlank
+        @NotBlank(message = "{telefone.obrigatorio}")
         String telefone,
 
-        @NotBlank
+        @NotBlank(message = "{cpf.obrigatorio}")
         String cpf,
 
-        @NotNull
+        @NotNull(message = "{endereco.obrigatorio}")
         @Valid
         DadosEndereco endereco
 ) {
