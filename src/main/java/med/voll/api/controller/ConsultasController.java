37aa9a3d4ddc4f01d46676761f8b147744e4064d
Kálley Wilkerson <kalleywra@gmail.com>
2023-09-25 20:45:33 -0300

    Criando meio de validar regras de negócio de um agendamento

--- src/main/java/med/voll/api/controller/ConsultasController.java
+++ src/main/java/med/voll/api/controller/ConsultasController.java
@@ -1,9 +1,13 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.Consulta;
 import med.voll.api.domain.consulta.ConsultaRepository;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import med.voll.api.service.AgendaDeConsultas;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,10 +23,22 @@ public class ConsultasController {
     @Autowired
     private ConsultaRepository repository;
 
+    @Autowired
+    private AgendaDeConsultas agenda;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
     @PostMapping
     @Transactional
     public ResponseEntity<?> agendar(@RequestBody @Valid DadosAgendamentoConsulta dados) {
-        System.out.println(dados);
+        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        var consulta = new Consulta(null, medico, paciente, dados.data());
+        agenda.validaAgendamento(consulta);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
     }
 
---
+++ src/main/java/med/voll/api/service/AgendaDeConsultas.java
@@ -0,0 +1,13 @@
+package med.voll.api.service;
+
+import med.voll.api.domain.consulta.Consulta;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaDeConsultas {
+
+    public void validaAgendamento(Consulta consulta) {
+
+    }
+
+}
